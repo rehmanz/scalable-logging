99414c3e271c3dc2c19c0b472c877a12bf5f4ee2
rehmanz <rehmanz@yahoo.com>
2022-06-25 05:50:31 -0700

    Debugging pytests.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
 import http.client
 import pytest
-
-from urllib.request import urlopen
+import requests
+# from urllib.request import urlopen
 
 @pytest.fixture(scope="session")
 def httpserver_listen_address():
     return ("localhost", 5000)
 
+
+def test_get_app_endpoint(httpserver):
+    assert requests.get(httpserver.url_for("/_healthz")).json() == "none"
 
 # def test_get_app_endpoint(httpserver):
 #     body = "user_agent"
@@ -16,10 +19,10 @@ def httpserver_listen_address():
 #         result = response.read().decode()
 #     assert body in result
 
-def test_get_healthz_endpoint(httpserver):
-    body = "HEALTH"
-    endpoint = "/_healthz"
-    httpserver.expect_request(endpoint).respond_with_data(body)
-    with urlopen(httpserver.url_for(endpoint)) as response:
-        result = response.read().decode()
-    assert body in result
+# def test_get_healthz_endpoint(httpserver):
+#     body = "HEALTH"
+#     endpoint = "/_healthz"
+#     httpserver.expect_request(endpoint).respond_with_data(body)
+#     with urlopen(httpserver.url_for(endpoint)) as response:
+#         result = response.read().decode()
+#     assert body in result
