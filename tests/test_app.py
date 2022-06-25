76cddb86ccd9a40b5c6b561b5c11d18e3fb351cb
rehmanz <rehmanz@yahoo.com>
2022-06-24 22:43:45 -0700

    Added pytests for endpoints.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,25 @@
-import sys
-sys.path.append('../app')
+import http.client
+import pytest
 
-from app import __version__
+from urllib.request import urlopen
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.fixture(scope="session")
+def httpserver_listen_address():
+    return ("localhost", 5000)
+
+
+def test_get_app_endpoint(httpserver):
+    body = "user_agent"
+    endpoint = ""
+    httpserver.expect_request(endpoint).respond_with_data(body)
+    with urlopen(httpserver.url_for(endpoint)) as response:
+        result = response.read().decode()
+    assert body in result
+
+def test_get_healthz_endpoint(httpserver):
+    body = "HEALTH"
+    endpoint = "/_healthz"
+    httpserver.expect_request(endpoint).respond_with_data(body)
+    with urlopen(httpserver.url_for(endpoint)) as response:
+        result = response.read().decode()
+    assert body in result
