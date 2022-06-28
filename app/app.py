19c84ba9013a7c1af621e36fb2194ce3b7659ea1
rehmanz <rehmanz@yahoo.com>
2022-06-28 10:04:48 -0700

    Debugging app.

--- app/app.py
+++ app/app.py
@@ -25,36 +25,36 @@ def hello():
     message = "Hello World!"
     logging.info(message)
     return message
-
-@app.errorhandler(404)
-def index(_):
-    remote_addr = request.remote_addr
-    xff = request.headers.get("X-Forwarded-For", "")
-    user_agent = request.headers.get("User-Agent", "")
-    path = request.path
-
-    entry = AccessEntry(ip=remote_addr, xff=xff, user_agent=user_agent, path=path)
-    db.session.add(entry)
-    db.session.commit()
-
-    entries = AccessEntry.query.order_by(AccessEntry.access_time.desc()).limit(10)
-    ret = [
-        {
-            "ip": entry.ip,
-            "xff": entry.xff,
-            "user_agent": entry.user_agent,
-            "path": entry.path,
-            "time": entry.access_time.isoformat(),
-        }
-        for entry in entries
-    ]
-
-    return json.dumps(ret)
-
-
-@app.route("/_healthz")
-def health():
-    return json.dumps({"status": "HEALTHY", "count": AccessEntry.query.count()})
+#
+# @app.errorhandler(404)
+# def index(_):
+#     remote_addr = request.remote_addr
+#     xff = request.headers.get("X-Forwarded-For", "")
+#     user_agent = request.headers.get("User-Agent", "")
+#     path = request.path
+#
+#     entry = AccessEntry(ip=remote_addr, xff=xff, user_agent=user_agent, path=path)
+#     db.session.add(entry)
+#     db.session.commit()
+#
+#     entries = AccessEntry.query.order_by(AccessEntry.access_time.desc()).limit(10)
+#     ret = [
+#         {
+#             "ip": entry.ip,
+#             "xff": entry.xff,
+#             "user_agent": entry.user_agent,
+#             "path": entry.path,
+#             "time": entry.access_time.isoformat(),
+#         }
+#         for entry in entries
+#     ]
+#
+#     return json.dumps(ret)
+#
+#
+# @app.route("/_healthz")
+# def health():
+#     return json.dumps({"status": "HEALTHY", "count": AccessEntry.query.count()})
 
 
 def main():
