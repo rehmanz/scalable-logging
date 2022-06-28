e2727dfe13d74a5783ff5a9098151fb7fe5960fb
rehmanz <rehmanz@yahoo.com>
2022-06-27 20:35:14 -0700

    Wrapped up postgres db infra.

--- terraform/database.tf
+++ terraform/database.tf
@@ -42,16 +42,19 @@ resource "aws_db_subnet_group" "slogging_db" {
 }
 
 resource "aws_db_instance" "slogging_db" {
-  identifier             = local.name
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "14.1"
-  username               = var.db_username
-  password               = var.db_password
+  identifier        = local.name
+  instance_class    = "db.t3.micro"
+  allocated_storage = 5
+  engine            = "postgres"
+  engine_version    = "14.1"
+  username          = var.db_username
+  password          = var.db_password
+
   db_subnet_group_name   = aws_db_subnet_group.slogging_db.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.slogging_db.name
   publicly_accessible    = true
   skip_final_snapshot    = true
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
